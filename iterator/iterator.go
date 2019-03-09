25fab7a7d10113910a6f20c6d84ad6d0c3e63d61
hhiroki <h-hiroki@users.noreply.github.com>
2019-03-10 02:12:49 +0900

    ⛳️動くところまで作成

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -2,37 +2,62 @@ package iterator
 
 import "fmt"
 
-// Book は本を表す構造体
-type Book struct {
-	name string
+type Aggregate interface {
+	Iterator() Iterator
 }
 
-// BookShelf は本棚を表す構造体。複数の本(Book)を所有可能
+type Iterator interface {
+	HasNext() bool
+	Next() interface{}
+}
+
+type BookShelfIterator struct {
+	BookShelf *BookShelf
+	Last      int
+}
+
+func (bookShelfIterator *BookShelfIterator) HasNext() bool {
+	if bookShelfIterator.Last >= len(bookShelfIterator.BookShelf.Books) {
+		return false
+	}
+	return true
+}
+
+func (bookShelfIterator *BookShelfIterator) Next() interface{} {
+	book := bookShelfIterator.BookShelf.Books[bookShelfIterator.Last]
+	bookShelfIterator.Last++
+	return book
+}
+
 type BookShelf struct {
 	Books []*Book
 }
 
-// AppendBook は本棚に本を格納するBookShelfのメソッド
 func (bookShelf *BookShelf) AppendBook(book *Book) {
 	bookShelf.Books = append(bookShelf.Books, book)
 }
 
-// Execute はプログラム実行用のメソッド
+func (bookShelf *BookShelf) Iterator() Iterator {
+	return &BookShelfIterator{BookShelf: bookShelf}
+}
+
+type Book struct {
+	name string
+}
+
 func Execute() {
-	// Bookを作成
-	firstBook := Book{name: "first book"}
-	secondBook := Book{name: "second book"}
-
-	fmt.Println(firstBook, secondBook) // デバッグ用
-
 	// BookShelfを作成
 	bookShelf := BookShelf{}
-	bookShelf.AppendBook(&firstBook)
-	bookShelf.AppendBook(&secondBook)
 
-	// デバッグ用
-	for _, book := range bookShelf.Books {
+	// BookをBookShelfに追加
+	bookShelf.AppendBook(&Book{name: "すごいHaskell本"})
+	bookShelf.AppendBook(&Book{name: "Java言語で学ぶデザインパターン入門"})
+	bookShelf.AppendBook(&Book{name: "リーダブルコード"})
+	bookShelf.AppendBook(&Book{name: "Goならわかるシステムプログラミング"})
+
+	iterator := bookShelf.Iterator()
+	for iterator.HasNext() {
+		book := iterator.Next()
 		fmt.Println(book)
 	}
-
 }
